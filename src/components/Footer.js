317fbd6f3a3770a39371694b97467379371ff86a
Elias Ferede <47753722+EliasB8@users.noreply.github.com>
2020-08-23 16:02:58 -0700

    Added twitter link for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Footer() {
-  return <p className="footer">Developed by Elias</p>;
+  return (
+    <p className="footer">
+      Developed by{" "}
+      <a
+        href="https://twitter.com/elias_ferede"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Elias
+      </a>
+    </p>
+  );
 }
 
 export default Footer;
