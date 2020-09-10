744611be5e72ca30738942918ce46f09d02c392d
Elias Ferede <feredeelias@gmail.com>
2020-09-10 16:10:06 -0700

    added git ignore, gh-pages, comments and build

---
+++ build/precache-manifest.c94bee2726474abd1326b73bcb670dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f623daf304399f907a8e2e4befc70c99",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "2d5c818abdc2d57dfb81",
+    "url": "/Random-Quote-Generator/static/css/main.7f639b20.chunk.css"
+  },
+  {
+    "revision": "b325212ca5eae3b2ae3a",
+    "url": "/Random-Quote-Generator/static/js/2.69cf4c7d.chunk.js"
+  },
+  {
+    "revision": "2d5c818abdc2d57dfb81",
+    "url": "/Random-Quote-Generator/static/js/main.b5c67ca6.chunk.js"
+  },
+  {
+    "revision": "6f23a534c95c8651034b",
+    "url": "/Random-Quote-Generator/static/js/runtime~main.618461f0.js"
+  }
+]);
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Quotes from "./Quotes";
 import Footer from "./Footer";
 
+// random color generator
 function rgbGenerator() {
   const r = Math.floor(Math.random() * 255) + 1;
   const g = Math.floor(Math.random() * 255) + 1;
@@ -10,6 +11,7 @@ function rgbGenerator() {
   return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
+//  css background color generator
 const background = () => {
   const color = rgbGenerator();
   return { backgroundColor: color, color: color };
@@ -20,14 +22,18 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
 
+    // Initial state
     this.state = {
       quote: {},
       randomQuote: {},
       color: color
     };
+
+    // bind he function to use the state
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // Make API request when component mount and store it in state
   componentDidMount() {
     fetch("https://type.fit/api/quotes")
       .then((response) => response.json())
@@ -40,6 +46,7 @@ class Main extends React.Component {
       });
   }
 
+  // on click of new quote generator a random quote id and color and display the quote
   handleClick() {
     const random = Math.floor(Math.random() * this.state.quote.length);
     this.setState((state) => ({
